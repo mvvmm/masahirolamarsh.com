97ff641e45b8bbac657ee036c0b92b8ee816fb86
Michael Morrison <morrim5@rpi.edu>
2021-07-30 10:44:16 -0400

    .

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,6 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Header() {
-  console.log();
   return (
     <div>
       <div className="h-auto p-4 lg:flex lg:flex-row-reverse lg:justify-between">
