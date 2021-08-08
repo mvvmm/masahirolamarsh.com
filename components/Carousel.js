91239ae748debd453f5c473529d31944a154f7ff
Michael Morrison <morrim5@rpi.edu>
2021-08-07 20:50:16 -0400

    add key

--- components/Carousel.js
+++ components/Carousel.js
@@ -2,7 +2,7 @@ export default function Carousel({ productData }) {
   return (
     <div className="flex">
       {productData.map((data) => (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+        <pre key={data.productID}>{JSON.stringify(data, null, 2)}</pre>
       ))}
     </div>
   );
