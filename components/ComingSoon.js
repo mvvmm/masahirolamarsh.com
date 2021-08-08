48163d380cba1bd9bacaf1a5632b40d6e5b84496
Michael Morrison <morrim5@rpi.edu>
2021-08-07 21:19:12 -0400

    h-screen -> absolute inset-0

--- components/ComingSoon.js
+++ components/ComingSoon.js
@@ -3,23 +3,25 @@ import { Instagram } from "@icons-pack/react-simple-icons";
 
 export default function ComingSoon() {
   return (
-    <a href="https://www.instagram.com/hirolamarsh">
-      <div className="h-screen flex flex-wrap content-center text-center">
-        <div className="mx-auto">
-          <Image
-            src="/img/coming-soon1.jpg"
-            width={642}
-            height={258}
-            alt="It's called F A M I L Y"
-            quality={100}
-          />
-          <div className="font-garamond border-white border">
-            <p className="font-light">masahirolamarsh.com</p>
-            <p className="font-light text-sm">coming soon</p>
+    <div class="absolute inset-0">
+      <a href="https://www.instagram.com/hirolamarsh">
+        <div className="h-full flex flex-wrap content-center text-center">
+          <div className="mx-auto">
+            <Image
+              src="/img/coming-soon1.jpg"
+              width={642}
+              height={258}
+              alt="It's called F A M I L Y"
+              quality={100}
+            />
+            <div className="font-garamond border-white border">
+              <p className="font-light">masahirolamarsh.com</p>
+              <p className="font-light text-sm">coming soon</p>
+            </div>
           </div>
         </div>
-      </div>
-    </a>
+      </a>
+    </div>
   );
 }
 
