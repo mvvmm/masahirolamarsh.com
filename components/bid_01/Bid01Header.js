53ac8c48f2d813e9f3b458bd35f807454c455277
Vance Morrison <vance@joinstatus.com>
2021-12-11 07:06:13 -0500

    fix header paragraph text

--- components/bid_01/Bid01Header.js
+++ components/bid_01/Bid01Header.js
@@ -3,14 +3,15 @@ export default function Bid01Header() {
     <div className="mx-auto mt-12 max-w-screen-md mb-4 px-2">
       <h1 className="text-7xl uppercase underline text-center">Bid I:</h1>
       <p className="m-4 text-lg text-justify leading-tight text-gray-300 font-garamond">
-        These three pieces are presented as sculptures. To keep things fair,
-        this is a blind auction lasting 1 week. The is a minimum bid, and any
-        value beyond is an investment in my success. All proceeds will go
+        These three pieces are presented as sculptures. This is a blind auction
+        lasting 1 week. Each piece has a minimum bid (reserve), and any bid
+        value beyond that is an investment in my success. All proceeds will go
         towards funding my first gallery show, and a capsule collection to be
-        released NYFW in February. All pieces come with the original
-        presentation, AND duplicate made to the specifications of the buyer size
-        in order to maintain the integrity of the original artwork. All of these
-        are to be understood as 1 of 1, with a personalized copy.
+        released in February for New York Fashion Week. All pieces come with the
+        original presentation, AND duplicate made to the specifications of the
+        buyer's size in order to maintain the integrity of the original artwork.
+        All of these are to be understood as 1 of 1, along with a personalized
+        copy.
       </p>
       <p className="text-right mr-12"> - M.</p>
     </div>
